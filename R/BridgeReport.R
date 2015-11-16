05d421024a23d91e8635bb11f8cb890b9db51ddb
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2015-11-16 17:24:05 +0900

    no message

---
+++ R/BridgeReport.R
@@ -0,0 +1,151 @@
+#Title: BridgeReport: Data visualization for RNA decay curve with shiny library
+#Auther: Naoto Imamachi
+#ver: 1.0.0
+#Date: 2015-11-16
+
+BridgeReport <- function(filename="BridgeR_4_Normalized_expression_dataset.txt",
+             group,
+             hour,
+             ComparisonFile,
+             SearchRow="symbol",
+             InforColumn=4)
+{
+    time_points <- length(hour) 
+    group_number <- length(group) 
+    input_file <- fread(filename, header=T)
+    
+    ###Prepare_file_infor###
+    comp_file_number <- NULL
+    for(a in 1:length(ComparisonFile)){
+        comp_file_number <- append(comp_file_number, which(group == ComparisonFile[a]))
+    }
+    setkeyv(input_file,SearchRow)
+    
+    ui <- fluidPage(
+        titlePanel("BridgeReport ver 0.1.0"),
+        
+        sidebarLayout(
+            position="left",
+            sidebarPanel(
+                helpText("Select a gene sysbol to examine. Information will be collected from your BRIC-seq dataset."),
+                textInput("text",
+                          label = "Input gene symbol",
+                          value = "Enter text..."),
+                sliderInput("range_x", 
+                            label = "X-axis(Time course):",
+                            min = 0, max = 12, value = c(0, 12)),
+                sliderInput("range_y", 
+                            label = "Y-axis(Relative RNA remaining):",
+                            min = 0.001, max = 10, value = c(0.1,1.5)),
+                selectInput("select", label = "Select decay curve calculation method", 
+                            choices = list("Raw" = 1,
+                                           "cutoff>=0.1" = 2,
+                                           "Except 1hr" = 3,
+                                           "Mix" = 4), selected = 1)
+            ),
+            
+            mainPanel(
+                #h1("test"),
+                #textOutput("text1"),
+                
+                fluidRow(
+                    column(width = 5,
+                           plotOutput("plot1",
+                                      click = "plot1_click",
+                                      brush = brushOpts(id = "plot1_brush")
+                           )
+                    )
+                )
+            )
+        )
+    )
+    
+    server <- function(input, output) {
+        
+        output$plot1 <- renderPlot({
+            data <- as.vector(as.matrix(input_file[input$text]))
+            gene_name <- as.character(data[2])
+            
+            ###Prepare_ggplot2###
+            p <- ggplot()
+            
+            flg <- 0
+            fig_color <- NULL
+            for(a in comp_file_number){
+                if(flg == 0){
+                    fig_color <- "black"
+                }else{
+                    fig_color <- "red"
+                }
+                infor_st <- 1 + (a - 1)*(time_points + InforColumn)
+                infor_ed <- (InforColumn)*a + (a - 1)*(time_points)
+                exp_st <- infor_ed + 1
+                exp_ed <- infor_ed + time_points
+                
+                exp <- as.numeric(data[exp_st:exp_ed])
+                time_point_exp_original <- data.frame(hour,exp)
+                
+                # For storing which rows have been excluded
+                #vals <- reactiveValues(
+                #    keeprows = rep(TRUE, nrow(time_point_exp_original))
+                #)
+                
+                time_point_exp <- time_point_exp_original[time_point_exp_original$exp > 0, ]
+                if(input$select == 1){
+                    
+                }else if(input$select == 2){
+                    time_point_exp <- time_point_exp[time_point_exp$exp >= 0.1, ]
+                }else if(input$select == 3){
+                    time_point_exp <- time_point_exp[time_point_exp$hour!=1,]
+                }else if(input$select == 4){
+                    time_point_exp <- time_point_exp[time_point_exp$hour!=1,]
+                    time_point_exp <- time_point_exp[time_point_exp$exp >= 0.1, ]
+                }
+                
+                p <- p + layer(data=time_point_exp, 
+                               mapping=aes(x=hour, y=exp), 
+                               geom="point",
+                               size=4,
+                               shape=19,
+                               colour=fig_color)
+                
+                data_point <- length(time_point_exp$exp)
+                if(!is.null(time_point_exp)){
+                    if(data_point >= 3){
+                        if(as.numeric(as.vector(as.matrix(time_point_exp$exp[1]))) > 0){
+                            model <- lm(log(time_point_exp$exp) ~ time_point_exp$hour - 1)
+                            model_summary <- summary(model)
+                            r_squared <- model_summary$r.squared
+                            
+                            #output$test1 <- renderText({ 
+                            #    paste("R2: ",as.character(r_squared),sep="")
+                            #})
+                            
+                            fig_data <- data.frame(hour=time_point_exp$hour)
+                            predicted <- as.numeric(as.vector(as.matrix(predict(model, fig_data))))
+                            fig_data$exp <- exp(as.vector(as.matrix(predicted)))
+                            p <- p + layer(data=fig_data,
+                                           mapping=(aes(x=hour, y=exp)),
+                                           geom="line",
+                                           size=1.2,
+                                           colour=fig_color)
+                        }
+                    }
+                }
+                flg <- 1
+            }
+            p <- p + ggtitle(gene_name)
+            p <- p + xlab("Time")
+            p <- p + ylab("Relative RPKM (Time0 = 1)")
+            p <- p + xlim(input$range_x[1],input$range_x[2])
+            ybreaks1 <- seq(0,10,0.1)[2:101]
+            ybreaks2 <- seq(0,0.1,0.01)[2:10]
+            ybreaks <- c(ybreaks1,ybreaks2)
+            p <- p + scale_y_log10(breaks=ybreaks, labels=ybreaks, limits=c(input$range_y[1],input$range_y[2]))
+            p
+        })
+    }
+    
+    app <- shinyApp(ui, server) # shinyApp() is also OK!
+    runApp(app)
+}
